532990c9cd25ef7e7be66c737cb540b981461824
unknown <janvier.bur@gmail.com>
2021-01-31 20:27:10 -0500

    add 02_even_fib.py

--- 01_mult_three_five.py
+++ 01_mult_three_five.py
@@ -8,6 +8,3 @@ def multiples(number):
         if i % 3 == 0 or i % 5 == 0:
             sum += i
     return sum
-
-
-print(multiples(1000))
---
+++ 02_even_fib.py
@@ -0,0 +1,12 @@
+fib1 = 1
+fib2 = 1
+result = 0
+sum = 0
+
+while result < 4000000:
+    if result % 2 == 0:
+        sum += result
+    result = fib1 + fib2
+    fib2 = fib1
+    fib1 = result
+print(sum)
